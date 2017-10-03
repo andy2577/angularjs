20df0a219efb8f49226f4906fa895ce6a5fda5a5
Andriy <andy25771@gmail.com>
2017-10-03 13:28:41 +0300

    working create and delete

--- app/controllers/app.js
+++ app/controllers/app.js
@@ -1,28 +1,48 @@
 var app = angular.module('eneida', []);
 
-
-
 app.controller('Eneida', function($scope, $http) {
 
+    $http.get('http://localhost:3000').then(function(response) {
+        // console.log(response.data);
+        $scope.poem = response.data;
+    });
 
-    $http.get('http://localhost:3000').
-        then(function(response) {
-            // console.log(response.data);
+    $scope.deleteQuote = function(event) {
+        let id = event.target.name;
+        var req = {
+            method: 'DELETE',
+            url: 'http://localhost:3000',
+            headers: {
+                "Content-Type": "application/json;charset=utf-8"
+            },
+            data: {
+                id: id
+            }
+        }
+        console.log(req);
+        $http(req).then(function(response) {
             $scope.poem = response.data;
         });
-    
-    $scope.deleteQuote = function(event){
-        let id = event.target.name;
-        let data = JSON.stringify({data:{id: id}});
-        console.log(data);
-        $http.delete('http://localhost:3000', data).
-            then(function(response){
-                $scope.poem = response.data;
-            });
+    }
+
+    $scope.quote = {};
+    $scope.createQuote = function() {
+        var req = {
+            method: 'POST',
+            url: 'http://localhost:3000',
+            headers: {
+                "Content-Type": "application/json;charset=utf-8"
+            },
+            data: $scope.quote
+        }
+        console.log(req);
+        $http(req).then(function(response) {
+            $scope.poem = response.data;
+        });
     }
 });
 
-app.directive('showRow', function(){
+app.directive('showRow', function() {
     return {
         templateUrl: 'view/showrow.html',
         replace: true
